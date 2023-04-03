b354845040a753b1d6731b78cf0950173375520f
Marissa Ovenshire <movenshire@tamu.edu>
2023-04-03 09:02:30 -0500

    renameing and create pool

--- excessReport.js
+++ excessReport.js
@@ -1,8 +1,7 @@
-const pool = require("./DB");
 const getExcessReport = (request,response) => {
     const start = request.query.start;
     if(!start){
-    response.status(400).json({error: 'Start timestamp not given'});
+        response.status(400).json({error: 'Start timestamp not given'});
     }
 
     /* full SQL command for inventory items (excludes calculation for multiple quantities per order)
@@ -21,7 +20,7 @@ const getExcessReport = (request,response) => {
     if(error) {
         throw error;
     }
-    let rows1 = results.rows;
+    let rows = results.rows;
     
       pool.query('SELECT * from inventory_item;',(error,results) => {
             if(error) {
@@ -42,8 +41,8 @@ const getExcessReport = (request,response) => {
             inventoryItemPercent.set(currItem, tenPercent);
         }
         
-        for(let i = 0;i<rows1.length;i++){
-            let currMenu = rows1[i];
+        for(let i = 0;i<rows.length;i++){
+            let currMenu = rows[i];
             let query = "SELECT * FROM relationship_menutoinventory_unitquantities where menuitemkey = '"+ currMenu.menuitem_key+"';";
             pool.query(query,(error,results) => {
             if(error) {
@@ -51,32 +50,25 @@ const getExcessReport = (request,response) => {
             }
             let rows3 = results.rows;
               let changeVal = rows3[0].unitquantity * currMenu.quantity +inventoryItemMap.get(rows3[0].inventoryitemkey);
-              console.log(changeVal);
+              //console.log(changeVal);
 
               //Map does not allow value overwriting so currently deleting to update the values
               //Probably need to just switch data type
-            //inventoryItemMap.set(rows3[0].inventoryitemkey, changeVal);
-
-            
-                inventoryItemMap.delete(rows3[0].inventoryitemkey);
+            if(inventoryItemMap.delete(rows3[0].inventoryitemkey) == true){
                 inventoryItemMap.set(rows3[0].inventoryitemkey, changeVal);
-                //console.log(inventoryItemMap.get(rows3[0].inventoryitemkey));
-            
+            }
             });
         }
-        console.log(inventoryItemMap);
 
         for(let key of inventoryItemMap.keys()){
-            //console.log(inventoryItemMap.get("Cup"));
             if(inventoryItemMap.get(key) < inventoryItemPercent.get(key)){ //&& inventoryItemMap.get(key)!=0){
             finalPairs.push({
                 name: key,
                 valueUsed: inventoryItemMap.get(key)
             });
             }
-            //console.log(inventoryItemMap.get(key))
+            response.status(200).json(finalPairs);
         }
-        response.status(200).json(finalPairs);
         });
         
     });
