2934a982a1fdef9767b4427a8e78596476f976e0
veritas4 <juan.carrascoluis@gmail.com>
2023-04-12 18:59:13 -0500

    Fixing X Report post testing

--- server/Xreport.js
+++ server/Xreport.js
@@ -1,29 +1,29 @@
 const pool = require("./DB");
 
 const getXreport = async (request, response) => {
-    // Query enerate the total sales since the last generated Z Report
     let t = Date.now();
     let totSales = 0;
     let num_orders = 0;
+    let t2 = new Date();
     
+    // Query the total sales since last generated Z Report
     let pricequery = `SELECT SUM(totalprice) FROM ordertable_test WHERE ordertimestamp >= (SELECT MAX(daysumm_timestamp) FROM day_summary) AND ordertimestamp <= to_timestamp(${t}/1000.0)`;
-    await pool.query(pricequery, (error, results) => {
-        if (error) {
-            throw error;
-        }
-        totSales = results.rows[0].sum;
-    });
-    console.log("X Report Total Sales: ", totSales);
+    let results = await pool.query(pricequery);
+    totSales += parseInt(results.rows[0].sum);
 
-    let numOrderQuery = `SELECT SUM(totalprice) FROM ordertable_test WHERE ordertimestamp >= (SELECT MAX(daysumm_timestamp) FROM day_summary) AND ordertimestamp <= to_timestamp(${t}/1000.0)`;
-    await pool.query(numOrderQuery, (error, results) => {
-        if (error) {
-            throw error;
-        }
-        num_orders = results.rows[0].count;
-    });
-    console.log("X Report Total Orders: ", num_orders);
-    response.status(200).json(totSales);
+    // Query the number of orders since last Z report
+    let numOrderQuery = `SELECT COUNT(totalprice) FROM ordertable_test WHERE ordertimestamp >= (SELECT MAX(daysumm_timestamp) FROM day_summary) AND ordertimestamp <= to_timestamp(${t}/1000.0)`;
+    results = await pool.query(numOrderQuery);
+    num_orders += parseInt(results.rows[0].count);
+
+    console.log("Date: ", t2, "\nTotal Sales: ",totSales, "\n# of Orders: ",num_orders);
+
+    const responseData = {
+        datetime : t2,
+        totalSales : totSales,
+        numOrders : num_orders
+    }
+    response.status(200).json(responseData);
 }
 
 module.exports = getXreport;
