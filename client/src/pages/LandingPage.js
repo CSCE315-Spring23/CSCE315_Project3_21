62d005f2a4dfd23f5453370b61ffa5691884a4d7
veritas4 <juan.carrascoluis@gmail.com>
2023-04-12 20:09:50 -0500

    Switch view button start

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,8 +1,11 @@
+import { Button } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 export default function LandingPage() {
     return (
       <div id="error-page">
         <h1>This is a WIP landing page</h1>
+        <Button variant="contained" component = {Link} to="/RestockReportPage">Restock Report Page</Button>
       </div>
     );
   }
