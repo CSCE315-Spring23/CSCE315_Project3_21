fa466ad9886a57b83f2bae42c853f7471b226005
Marissa Ovenshire <movenshire@tamu.edu>
2023-04-26 13:17:12 -0500

    minor post execss merge changes

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactWeatherComponent from '../components/ReactWeatherComponent';
 
 export default function LandingPage() {
-  const navigate = useNavigate();
     return (
 
       <div id="landing-page">
