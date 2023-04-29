3e9087881a0248dd18278bd59a7ef100609b3921
chandlerg1224 <chandlermgerold@gmail.com>
2023-04-29 11:45:33 -0500

    not working yet

---
+++ client/src/components/SalesReportTable.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import MaterialReactTable from 'material-react-table';
+import axios from 'axios';
+import Moment from 'react-moment';
+import moment from 'moment/moment.js';
+
+const columns = [
+    {
+        accessorKey: 'id',
+        header: 'ID',
+    },
+    {
+        accessorKey: 'order_key',
+        header: 'Order Key',
+    },
+    {
+        accessorKey: 'menuitem_key',
+        header: 'Menu Item Key',
+    },
+    {
+        accessorKey: 'quantity',
+        header: 'Quantity',
+    },
+];
+
+const config = {
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+    }
+};
+
+export default class SalesReportTable extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            start: this.props.startVal,
+            data : [],
+        };
+    }
+    
+    componentDidMount() {
+        var t = new Date(this.state.start);
+        var t2 = new Date(this.state.end);
+        var format = moment(t).format("YYYY-MM-DD hh:mm:ss"); 
+        var format2 = moment(t2).format("YYYY-MM-DD hh:mm:ss");
+        var str = `http://localhost:3001/SalesReport?start= `+format+'&end='+format2;
+        axios.get(str, config)
+            .then(res => {
+                const reportData = res.data;
+                this.setState({data: reportData});
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
+    render(){
+        console.log(this.state.data.at(0));
+        return <MaterialReactTable columns={columns} data={this.state.data} />;
+    }
+};
--- client/src/pages/SalesReportPage.js
+++ client/src/pages/SalesReportPage.js
@@ -1,176 +1,97 @@
-import React, { useEffect } from "react";
+import ExcessReportTable from '../components/ExcessReportTable.js';
+import React, {useState} from 'react';
 
-import { styled } from "@mui/material/styles";
-//import Box from '@mui/material/Box';
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Unstable_Grid2";
-import MainAppBar from "../components/MainAppBar.js";
-import {
-  Button,
-  Card,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from "@mui/material";
-import { width } from "@mui/system";
-import axios from "axios";
+import { styled, ThemeProvider, createTheme } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import MainAppBar from '../components/MainAppBar.js';
+import { Button } from '@mui/material';
+import { Typography } from '@mui/material';
+
+import { DateTimePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import SalesReportTable from '../components/SalesReportTable.js';
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#fff",
+  backgroundColor: '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: "center",
+  textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
-const SearchUI = ({ search }) => {
-  const [startDate, setStartDate] = React.useState(null);
-  const [endDate, setEndDate] = React.useState(null);
-  
+const Title = styled(Paper)(({ theme }) => ({
+  ...theme.typography.heading,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: 'black',
+}));
 
-  const [error1, setError1] = React.useState(null);
-  const [error2, setError2] = React.useState(null);
-  const [error3, setError3] = React.useState(null);
+const cfa_theme = createTheme({
+  palette: {
+      primary: {
+          main: '#E51636',
+      },
+      secondary: {
+          main: '#E51636',
+      },
+  },
+});
 
-  const handleSearch = () => {
-    
-    if (!error1 && !error2) {
-      if (startDate == null || endDate == null) {
-        setError3("Please choose valid date first");
-        return;
-      }
-      setError3(null);
-      search(startDate, endDate);
-    } else {
-      setError3("Please choose valid date");
-    }
-  };
+export default function SalesReportPage(){
+  const [start, setStart] = useState('');
+  const [end, setEnd] = useState('');
+  const [submit, setSubmit]= useState(false);
+  const [submit2, setSubmit2] = useState(false);
+    return (
+      <ThemeProvider theme={cfa_theme}>
+        <Grid container spacing={2}>
+          <Grid xs={12}>
+            <MainAppBar>
+            </MainAppBar>
+          </Grid>
+          <Grid xs={12}>
+            <Grid>
+              <LocalizationProvider dateAdapter={AdapterDayjs} adaprterLocale={'en-gb'}>
+                <DateTimePicker 
+                  value={start}
+                  onChange={(e)=>setStart(e)}
+                  format = "YYYY-MM-DD HH:mm:ss"
+                />
+              </LocalizationProvider>
 
-  
-
-  return (
-    <Card
-      style={{ width: "50%", padding: "20px", maxWidth: "700px" }}
-      variant="elevation"
-    >
-      <Grid container>
-        <Grid xs={6}>
-          <Item>
-            <TextField
-              style={{ width: "100%" }}
-              onChange={(e) => {
-
-                  setError1(null);
-                  setStartDate(e.target.value);
-                }
-              }
-              label="Starting Date"
-              type="string"
-              error={error1 == null ? false : true}
-              helperText={error1}
-            />
-          </Item>
-        </Grid>
-
-        <Grid xs={6}>
-          <Item>
-            <TextField
-              style={{ width: "100%" }}
-              onChange={(e) => {
-                
-                  setError2(null);
-                  setEndDate(e.target.value);
-                
-              }}
-              error={error2 == null ? false : true}
-              helperText={error2}
-              label="Ending Date"
-              type="string"
-            />
-          </Item>
-        </Grid>
-
-        <Grid xs={12}>
-          <Item>
-            <InputLabel>Sales Report Table</InputLabel>
+              <LocalizationProvider dateAdapter={AdapterDayjs} adaprterLocale={'en-gb'}>
+                <DateTimePicker 
+                  value={end}
+                  onChange={(e)=>setEnd(e)}
+                  format = "YYYY-MM-DD HH:mm:ss"
+                />
+              </LocalizationProvider>
+            <Button
+              variant = "contained"
+              onClick ={()=>setSubmit(true)}
+              >submit
+            </Button>
             
-          </Item>
+            <Grid xs = {1}></Grid>
+            <Button
+              variant = "contained"
+              onClick = {()=> window.location.reload()}
+            > Reset to pick new date 
+              </Button>
+            </Grid>
+            <Item>
+            <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                Sales Report Table
+            </Typography>
+              {submit
+                ?<SalesReportTable startVal = {start}/>
+                : "No date selected"}, <SalesReportTable endVal = {end}/>
+              
+            </Item>
+          </Grid>
         </Grid>
-      </Grid>
-
-      <center>
-        <Button
-          style={{ padding: "10px", margin: "5px" }}
-          onClick={handleSearch}
-        >
-          Get Result
-        </Button>{" "}
-        <br></br>
-        <span style={{ color: "red", fontSize: "14px" }}>{error3}</span>
-      </center>
-    </Card>
-  );
-};
-
-function SalesReportPage() {
-  const [data, setData] = React.useState([]);
-  const [searching, setSearching] = React.useState(false);
-
-  const search = (start, end) => {
-    setSearching(true);
-    setData([]);
-    axios
-      .get(
-        `http://localhost:3001/salesReport?start=${start}&end=${end}`
-      )
-      .then((res) => {
-        setSearching(false);
-        setData(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-        searching(false);
-      });
-  };
-
-  return (
-    <div className="SalesReportPage">
-      <Grid container spacing={2}>
-        <Grid xs={12}>
-          <MainAppBar></MainAppBar>
-        </Grid>
-
-        <Grid xs={12}>
-          <center>
-            <SearchUI search={search} />
-          </center>
-        </Grid>
-
-        <Grid xs={12}>
-          <center>
-            <h2>
-              {searching
-                ? "Getting Result...."
-                : data.length == 0
-                ? "No Result Found!"
-                : "Result"}
-            </h2>
-          </center>
-          {searching || data.length == 0 ? null : (
-            <center>
-              <Item style={{ width: "50%", maxWidth: "500px" }}>
-                <table border={"black"} style={{ width: "100%" }}>
-                  
-                  
-                </table>
-              </Item>
-            </center>
-          )}
-        </Grid>
-      </Grid>
-    </div>
+    </ThemeProvider>
   );
 }
-
-export default SalesReportPage;
